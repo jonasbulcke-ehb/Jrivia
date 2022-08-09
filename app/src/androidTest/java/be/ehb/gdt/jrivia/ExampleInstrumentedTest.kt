48e8dad2cc5acd3941fdcc5bc370db84d05f27eb
Jonas Bulcke <jonas.bulcke@student.ehb.be>
2022-08-09 23:19:46 +0200

    test: check if dailyQuestDatabase is seeded successfully

--- app/src/androidTest/java/be/ehb/gdt/jrivia/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/be/ehb/gdt/jrivia/ExampleInstrumentedTest.kt
@@ -2,6 +2,13 @@ package be.ehb.gdt.jrivia
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import be.ehb.gdt.jrivia.models.DailyQuest
+import be.ehb.gdt.jrivia.room.DailyQuestRepository
+import be.ehb.gdt.jrivia.room.JriviaRoomDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,5 +27,21 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("be.ehb.gdt.jrivia", appContext.packageName)
+    }
+
+    @Test
+    fun isDailyQuestTableSeeded() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val coroutineScope = CoroutineScope(SupervisorJob())
+        val database = JriviaRoomDatabase.getDatabase(appContext, coroutineScope)
+        val repository = DailyQuestRepository(database.dailyQuestDao())
+
+        val quest = DailyQuest(1,"It recently moved ahead of Chicago to become our 2nd largest", "Los Angeles", 100)
+
+        coroutineScope.launch {
+            repository.getLastQuest().collect {
+                assertEquals(quest, it)
+            }
+        }
     }
 }
